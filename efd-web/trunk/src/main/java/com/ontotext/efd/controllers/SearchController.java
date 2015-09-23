a1c3d5533fbae44aab84216274a4b57bced2dbc8
boyan.simeonov <boyan.simeonov@10f27cf6-d16d-49e4-bdc3-02f3720e5c59>
2015-09-23 12:22:23 +0000

    Search

---
+++ efd-web/trunk/src/main/java/com/ontotext/efd/controllers/SearchController.java
@@ -0,0 +1,45 @@
+package com.ontotext.efd.controllers;
+
+import com.ontotext.efd.services.SearchQueryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by boyan on 15-9-18.
+ */
+@Controller
+@RequestMapping("/search")
+public class SearchController {
+
+    @Value("${search.query}")
+    private String searchQuery;
+
+    @Autowired
+    SearchQueryService searchQueryService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public void search(HttpServletRequest request, @RequestParam("query") String query, @RequestParam(value = "rows", required = false, defaultValue = "10") String rows) {
+        System.out.println(rows);
+        System.out.println(searchQuery);
+        searchQueryService.sayHi();
+
+
+    }
+
+    public void facetSearch(HttpServletRequest request,
+                            @RequestParam("query") String query,
+                            @RequestParam(value = "rows", required = false, defaultValue = "10") String rows,
+                            @RequestParam(value = "type", required = false) String [] type,
+                            @RequestParam(value = "country", required = false) String [] country,
+                            @RequestParam(value = "provider", required = false) String [] provider,
+                            @RequestParam(value = "language", required = false) String [] language){
+
+    }
+
+}

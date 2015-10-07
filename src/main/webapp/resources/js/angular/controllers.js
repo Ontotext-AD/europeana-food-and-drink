01a2f7dcdce72c83da80de0325b45b923c41c476
rostislav.kirchev <rostislav.kirchev@10f27cf6-d16d-49e4-bdc3-02f3720e5c59>
2015-10-07 06:04:06 +0000

    Create Search results page https://jira.ontotext.com/browse/AMBR-144

--- src/main/webapp/resources/js/angular/controllers.js
+++ src/main/webapp/resources/js/angular/controllers.js
@@ -3,7 +3,7 @@
  */
 
 
-define(['angularJS'], function(){
+define(['angular'], function(){
     var efdControllers = angular.module('efdApp.controllers', [
         'ngRoute'
     ]);
@@ -13,14 +13,68 @@ define(['angularJS'], function(){
         $scope.test = 'MainCtrl';
     }]);
 
-    efdControllers.controller('SearchCtrl', ["$scope", function($scope) {
-        $scope.test = 'SearchCtrl';
-    }]);
+    efdControllers.controller('SearchCtrl',[
+        "$scope",
+        "$rootScope",
+        '$http',
+        '$location',
+        function($scope, $rootScope, $http, $location) {
 
-    efdControllers.controller('ResultCtrl', ["$scope", '$routeParams', function($scope, $routeParams) {
-        $scope.params = $routeParams;
-        $scope.test = 'ResultCtrl';
-    }]);
+            $scope.test = 'SearchCtrl';
+            $scope.search = function(){
+                $location.path('/search').search({query: $scope.searchQuery});
+            }
+        }
+    ]);
+
+    efdControllers.controller('ResultCtrl', [
+        "$scope",
+        '$routeParams',
+        '$http',
+        '$location',
+        'localStorageService',
+        function($scope, $routeParams, $http, $location, localStorageService) {
+            $scope.searchData = $location.search();
+            console.log($scope.searchData);
+
+            $scope.filtersCategories = [
+                {title: 'By media type', searchString: '', data: []},
+                {title: 'By language of description', searchString: 'language', data: []},
+                {title: 'By providing country', searchString: '', data: []},
+                {title: 'Can I use it?', searchString: '', data: []},
+                {title: 'By copyright', searchString: '', data: []},
+                {title: 'By provider', searchString: 'provider', data: []},
+                {title: 'By data provider', searchString: 'dataProvider', data: []}
+            ]
+            $scope.hasFacets = function(filter){
+                return filter.data.length > 0;
+            }
+            $scope.params = $routeParams;
+            $scope.test = 'ResultCtrl';
+            $scope.search = function(){
+                $http.get('/rest/search?query=' + $scope.searchQuery).
+                    then(function(response) {
+                        $scope.status = response.status;
+                        $scope.data = response.data;
+                        for(var i = 0; i < $scope.filtersCategories.length; i++){
+                            if ($scope.data.facets[$scope.filtersCategories[i].searchString]){
+                                $scope.filtersCategories[i].data = $scope.data.facets[$scope.filtersCategories[i].searchString];
+                            } else {
+                                $scope.filtersCategories[i].data = [];
+                            }
+                        }
+                        console.log($scope.filtersCategories);
+                    }, function(response) {
+                        $scope.data = response.data || "Request failed";
+                        $scope.status = response.status;
+                    });
+            }
+            if($scope.searchData.query) {
+                $scope.searchQuery = $scope.searchData.query;
+                $scope.search();
+            }
+        }
+    ]);
 
     efdControllers.controller('ResourceCtrl', ["$scope", '$routeParams', function($scope, $routeParams) {
 

7dafcbc85aafbe51ad526577701f19a47bdc844c
rostislav.kirchev <rostislav.kirchev@10f27cf6-d16d-49e4-bdc3-02f3720e5c59>
2015-10-13 13:09:39 +0000

    Create Search results page https://jira.ontotext.com/browse/AMBR-144

--- efd-web/trunk/src/main/webapp/resources/js/angular/controllers.js
+++ efd-web/trunk/src/main/webapp/resources/js/angular/controllers.js
@@ -30,9 +30,10 @@ define(['angular'], function(){
         '$routeParams',
         '$http',
         '$location',
+        '$timeout',
         'localStorageService',
         'toastr',
-        function($scope, $routeParams, $http, $location, localStorageService ,toastr) {
+        function($scope, $routeParams, $http, $location, $timeout, localStorageService ,toastr) {
             $scope.loader = true;
             $scope.countLoader = true;
             $scope.searchData = $location.search();
@@ -51,14 +52,15 @@ define(['angular'], function(){
 
 
             $scope.filtersCategories = [
-                {id: 0, title: 'By media type', searchString: 'mediaType', data: [], isDisabled: false},
-                {id: 1, title: 'By language of description', searchString: 'language', data: [], isDisabled: false},
-                {id: 2, title: 'By providing country', searchString: '', data: [], isDisabled: false},
-                {id: 3, title: 'Can I use it?', searchString: '', data: [], isDisabled: false},
-                {id: 4, title: 'By copyright', searchString: '', data: [], isDisabled: false},
-                {id: 5, title: 'By provider', searchString: 'provider', data: [], isDisabled: false},
-                {id: 6, title: 'By data provider', searchString: 'dataProvider', data: [], isDisabled: false}
+                {id: 0, title: 'By media type', searchString: 'mediaType', data: [], isDisabled: true},
+                {id: 1, title: 'By language of description', searchString: 'language', data: [], isDisabled: true},
+                {id: 2, title: 'By providing country', searchString: '', data: [], isDisabled: true},
+                {id: 3, title: 'Can I use it?', searchString: '', data: [], isDisabled: true},
+                {id: 4, title: 'By copyright', searchString: '', data: [], isDisabled: true},
+                {id: 5, title: 'By provider', searchString: 'provider', data: [], isDisabled: true},
+                {id: 6, title: 'By data provider', searchString: 'dataProvider', data: [], isDisabled: true}
             ]
+
             $scope.hasFacets = function(filter){
                 return filter.data.length > 0;
             }
@@ -77,7 +79,7 @@ define(['angular'], function(){
                     then(function(response) {
                         $scope.count = response.data;
                         $scope.totalPages = Math.ceil($scope.count/$scope.limit);
-                        $scope.page = $scope.offset/$scope.limit + 1;
+                        $scope.results = {page: $scope.offset/$scope.limit + 1};
                         $scope.countLoader = false;
                     }, function(){
                         $scope.count = 0;
@@ -91,13 +93,16 @@ define(['angular'], function(){
                 $scope.loader = true;
                 if (newSearch) {
                     $scope.activeFilters = [];
+                    $scope.offset = 0;
                     $scope.searchData = {
                         query: $scope.searchQuery,
-                        limit: $scope.limit
+                        limit: $scope.limit,
+                        offset: $scope.offset
                     }
                     $location.search($scope.searchData);
                 }
                 $scope.searchData = $location.search();
+                $scope.setActiveFilters();
                 var searchString = '';
                 for(index in $scope.searchData) {
                     if (searchString != '') {
@@ -122,7 +127,6 @@ define(['angular'], function(){
                                 $scope.filtersCategories[i].isDisabled = true;
                             }
                         }
-                        console.log($scope.filtersCategories);
                         $scope.loader = false;
                         $scope.getCount(searchString);
                     }, function(response) {
@@ -131,24 +135,96 @@ define(['angular'], function(){
                         $scope.loader = false;
                     });
             }
-            var t;
-            $scope.addRemoveFilter = function(categoryIndex, facetIndex){
-                debugger;
-                var newFilter = {
-                    catIndex: categoryIndex,
-                    facetName: $scope.filtersCategories[categoryIndex].data[facetIndex].facetName,
-                    facetValue: $scope.filtersCategories[categoryIndex].data[facetIndex].facetValue
+
+            $scope.setActiveFilters = function(){
+                for (index in $scope.searchData) {
+                    var exist = false;
+                    for (var i = 0; i < $scope.filtersCategories.length; i++){
+                        if (index == $scope.filtersCategories[i].searchString) {
+                            exist = true;
+                        }
+                    }
+                    if (exist){
+
+                        var filterArr = $scope.searchData[index].split(',');
+
+                        for (var i = 0; i < filterArr.length; i++){
+                            var newFilter = {
+                                categoryName: index,
+                                facetName: filterArr[i]
+                            }
+                            $scope.activeFilters.push(newFilter);
+                        }
+                    }
                 }
-                $scope.activeFilters.push(newFilter);
-                clearTimeout(t);
-                t = setTimeout($scope.search(), 2000);
             }
 
+            //Remove filter only from the Selected filters
+            $scope.removeFilter = function(categoryName, facetName){
+                if ($scope.searchData[categoryName]){
+                    var filterArr = $scope.searchData[categoryName].split(',');
+                    for (var i = 0; i < filterArr.length; i++){
+                        if (filterArr[i] == facetName){
+                            filterArr.splice(i, 1);
+                        }
+                    }
+                    if (filterArr.length == 0){
+                        delete $scope.searchData[categoryName];
+                    } else {
+                        $scope.searchData[categoryName] = filterArr.join(',');
+                    }
+                    $location.search($scope.searchData);
+                }
+            }
+
+            //Add/Remove filter from facets categories menu
+            var addRemoveFilterTimeout;
+            $scope.addRemoveFilter = function(categoryIndex, facetIndex){
+                $timeout.cancel(addRemoveFilterTimeout);
+                if ($scope.filtersCategories[categoryIndex].data[facetIndex].checked === false) {
+                    $scope.removeFilter($scope.filtersCategories[categoryIndex].searchString, $scope.filtersCategories[categoryIndex].data[facetIndex].facetName);
+                    return;
+                }
+
+                var newFilter = {
+                    categoryName: $scope.filtersCategories[categoryIndex].searchString,
+                    facetName: $scope.filtersCategories[categoryIndex].data[facetIndex].facetName
+                }
+                $scope.activeFilters.push(newFilter);
+                if(angular.isUndefined($scope.searchData[$scope.filtersCategories[categoryIndex].searchString])){
+                    $scope.searchData[$scope.filtersCategories[categoryIndex].searchString] = newFilter.facetName;
+                } else {
+                    var temp = $scope.searchData[$scope.filtersCategories[categoryIndex].searchString].split(',');
+                    temp.push(newFilter.facetName);
+                    $scope.searchData[$scope.filtersCategories[categoryIndex].searchString] = temp.join(",");
+                }
+
+                $scope.searchData.offset = 0;
+                addRemoveFilterTimeout = $timeout(function(){
+                    $location.search($scope.searchData);
+                }, 1000);
+
+                $scope.$on("$destroy", function(event) {
+                    $timeout.cancel(addRemoveFilterTimeout);
+                });
+            }
+
+            var changePageTimeout;
             $scope.changePage = function(){
-                $scope.offset = $scope.limit * ($scope.page - 1);
-                $scope.searchData.offset = $scope.offset;
-                $location.search($scope.searchData);
-                $scope.search();
+                $timeout.cancel(changePageTimeout);
+                changePageTimeout = $timeout(function(){
+                    $scope.offset = $scope.limit * ($scope.results.page - 1);
+                    $scope.searchData.offset = $scope.offset;
+                    $location.search($scope.searchData);
+                }, 2000);
+
+                $scope.$on("$destroy", function(event) {
+                    $timeout.cancel(changePageTimeout);
+                });
+            }
+
+            $scope.loadResource = function(resource){
+                $location.path('/resource/'+ encodeURIComponent(resource)).search($scope.searchData);
             }
 
             if($scope.searchData.query) {

83d8c2e4d3cd25cea5315c6872a0603d8f92caea
rostislav.kirchev <rostislav.kirchev@10f27cf6-d16d-49e4-bdc3-02f3720e5c59>
2016-07-19 21:16:42 +0000

    Fix bug with wrong markers on map after second search.

---
+++ efd-web/trunk/src/main/webapp/resources/js/angular/factory.js
@@ -0,0 +1,25 @@
+
+
+define(['angular'], function(){
+    var factories = angular.module('efdApp.factories', []);
+
+    factories.factory('mapFactory', mapFactory);
+
+    function mapFactory() {
+        var factory = {
+            markerClusterer: '',
+            markers: [],
+            clearMarkers: clearMarkers,
+        }
+
+        return factory;
+
+        function clearMarkers() {
+            factory.markers = [];
+            factory.markerClusterer.clearMarkers();
+            factory.markerClusterer.setMap(null);
+        }
+    }
+
+    return factories;
+});
